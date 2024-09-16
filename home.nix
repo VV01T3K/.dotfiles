3575a70c06b336679dbb10a2e49775365780e01a
VV01T3K <wojteks.access@gmail.com>
2024-09-16 03:10:23 +0200

    zsh in nix-shell

--- home.nix
+++ home.nix
@@ -123,6 +123,20 @@ in
         "aliases" # the aliases plugin provides many aliases and a few useful functions.
       ];
     };
+
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
+
   };
 
   programs.git = {
